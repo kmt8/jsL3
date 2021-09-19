f96174b58b51e52058b8d23e43450843eb3dbbb3
Kyaw Myat Thu <kyawmyatthu.8t@gmail.com>
2021-09-19 18:53:12 +0630

    19 Sep

---
+++ feedback/js/app.js
@@ -0,0 +1,10 @@
+// UI
+const panel = document.querySelector('#panel');
+const ratingcontainer = document.querySelector('.rating-container');
+const ratings = document.querySelectorAll('.rating');
+const sendbtn = document.querySelector('#send');
+
+
+panel.addEventListener('click',(e)=>{
+    
+});
