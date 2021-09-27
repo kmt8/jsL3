087c5010309791b1b3386dc50ee272254721d969
Kyaw Myat Thu <kyawmyatthu.8t@gmail.com>
2021-09-27 20:24:20 +0630

    27 Sep

--- draganddrop/js/app.js
+++ draganddrop/js/app.js
@@ -2,22 +2,77 @@
 const picdiv = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
 
-
 picdiv.addEventListener('dragstart',dragstart);
 picdiv.addEventListener('dragend',dragend);
 
-
-// Drag Start
+// DRAG START
 function dragstart(){
-    // console.log('dragstart is working');
+    console.log('drag start is working');
 
     // this.classList.add('hold');
+
     this.className += " hold";
+
+    setTimeout(()=>{
+        this.className = 'invisible';
+    },0);
 }
 
-// Drag End
-function dragend(){
-    // console.log('dragend is working');
+// console.log(empties);
 
-    this.className = "fill";
+// empties.forEach(empty=>{
+//    console.log(empty);
+//
+//    empty.addEventListener('dragover',dragover);
+//    empty.addEventListener('dragenter',dragenter);
+//    empty.addEventListener('dragleave',dragleave);
+//    empty.addEventListener('drop',dragdrop);
+// });
+
+// => ES6 .can object5-specific iterator value
+for(const empty of empties){
+    console.log(empty);
+        empty.addEventListener('dragover',dragover);
+        empty.addEventListener('dragenter',dragenter);
+        empty.addEventListener('dragleave',dragleave);
+        empty.addEventListener('drop',dragdrop);
+}
+
+// => numberable property name of an object
+// for(const empty in empties){
+//     console.log(empty);
+// }
+
+// DRAG END
+function dragend(){
+    console.log('drag end is working');
+
+    this.className = 'fill';
+}
+
+function dragover(e){
+    console.log('drag over');
+    e.preventDefault();
+}
+
+function dragenter(e){
+    console.log('drag enter');
+    e.preventDefault();
+
+    this.className += ' hovered';
+}
+
+function dragleave(e){
+    console.log('drag leave');
+    e.preventDefault();
+
+    this.className = 'empty';
+}
+
+function dragdrop(e){
+    console.log('drag drop');
+    e.preventDefault();
+
+    this.className = 'empty';
+    this.appendChild(picdiv);
 }
